4af4617626905f0e64f3004162122562099bc4bc
William Alvarez <111912100+wilyyyy@users.noreply.github.com>
2023-08-02 18:48:48 -0700

    copilot leaking the answer :sob:

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ Write an algo that takes an array of numbers as input and calculates the sum of 
 Define the time complexity of that algo and determine the lowest possible time complexity for this problem
 */
 
+// O(n) time complexity (Linear)
+function SumNumbers(n) {
+  let sum = 0;
+  for (let i = 0; i < n.length; i++) {
+    sum += n[i];
+  }
+}
