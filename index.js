3b3bc69e8a8a8cef2ecb56412826fe114c4b5e36
William Alvarez <111912100+wilyyyy@users.noreply.github.com>
2023-08-02 18:18:17 -0700

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+/*
+Write an algo that takes an array of numbers as input and calculates the sum of those numbers
+
+Define the time complexity of that algo and determine the lowest possible time complexity for this problem
+*/
+
